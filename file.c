d21cb1f7d5eeee12a1f828724d4c29b88a5f7565
Roberta <robemac@hotmail.it>
2018-12-13 13:52:01 +0100

    Sottrazione

--- file.c
+++ file.c
@@ -2,9 +2,11 @@
 
 void main()
 {
-int a, b, sum, product;
+int a, b, sum, product, diff;
 sum=a+b;
 product=a*b;
+diff=a-b;
 printf("La somma e':%d\n", sum);
 printf("Il prodotto e':%d\n", product);
+printf("La diff e':%d\n", diff);
 }
